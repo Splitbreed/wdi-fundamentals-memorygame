b5dd737e4c0631bb9249d40b0f2247c5f4c5e0da
Adam Youngblood <ayoungbl0d@gmail.com>
2016-08-21 15:40:33 -0500

    added basic logic check for mathcing cards

--- starter-code/main.js
+++ starter-code/main.js
@@ -2,3 +2,9 @@ var cardOne="king";
 var cardTwo="king";
 var cardThree="queen";
 var cardFour="queen";
+
+if (cardOne == cardTwo){
+	alert('You found a match!');
+} else {
+	alert('Sorry, try again.');
+}
